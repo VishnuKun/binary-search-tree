376a968af12d08a7d1fbdcc8b0820a2ca44a83db
Vishnu <vishnukun954AV@protonmail.com>
2023-07-02 21:36:18 +0530

    Established links with other files plus refactored.

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,15 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+
 class Tree
-    attr_accessor :root
-    def initialize
-        @root = nil
-    end
-    # method to convert array to BST    
-    # and return root of tree
-    def array_to_BST(nums)
-        # if array.empty? return nil
-        # else 
-          # find middle element
-          # make it the root
-          
-    end
+  attr_accessor :root
+
+  def initialize
+    @root = nil
+  end
+
+  # method to convert array to BST
+  # and return root of tree
+
+  def array_to_BST(nums)
+    # if array.empty? return nil
+    return nil if nums.nil?
+
+    # else
+    # find middle element
+    # make it the root
+    # divide array into two parts
+    # left and right from the middle element
+    # repeat process using recursion
+    # make middle of left, root
+    # make middle of right, root
+    # return pre-order of tree
+  end
 end
