b9c31a1e482aa89e4d22523e3d096f5ad7c97209
Vishnu <vishnukun954AV@protonmail.com>
2023-07-08 13:18:46 +0530

    Initialized find and insert method.

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,21 +5,59 @@ require_relative 'node'
 class Tree
   attr_accessor :root
 
-  def initialize
-    @root = nil
+  def initialize(array = nil)
+    @root = build_tree(array)
   end
 
-  # method to convert array to BST
+  # convert array to BST
 
-  def build_tree(nums)
-    return nil if nums.nil? || nums.empty?
+  def build_tree(arr)
+    return nil if arr.nil? || arr.empty?
 
-    n = nums.length
+    n = arr.size
     mid = n.div(2)
-    root = Node.new(nums[mid])
-    root.left = build_tree(nums[...mid])
-    root.right = build_tree(nums[mid + 1..])
-    
-    root
+
+    node = Node.new(arr[mid])
+    node.left = build_tree(arr[...mid])
+    node.right = build_tree(arr[mid + 1..])
+    node
+  end
+
+  def insert(value)
+    current = @root
+    if current.nil?
+      @root = Node.new(value)
+    else
+      while current
+        if current.left.nil? && current.data > value
+          current.left = Node.new(value)
+          break
+        elsif current.right.nil? && current.data < value
+          current.right = Node.new(value)
+          break
+        else
+          current = if current.data > value
+                      current.left
+                    else
+                      current.right
+                    end
+        end
+      end
+    end
+  end
+
+  def find(value)
+    return false if @root.nil?
+
+    current = @root
+    while current
+      return true if current.data === value
+
+      current = if current.data > value
+                  current.left
+                else
+                  current.right
+                end
+    end
   end
 end
